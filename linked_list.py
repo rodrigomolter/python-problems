46c2f4764cdbfbdc36b65de2187df7e07bbab69e
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-05-29 23:11:51 -0300

    feat: add linked list

---
+++ linked_list.py
@@ -0,0 +1,22 @@
+import sys
+
+class Node():
+  def __init__(self, value, parent = None):
+    self.value = value
+    self.parent = parent
+
+  def __str__(self) -> str:
+    return f"Node({self.value})->{self.parent}"
+
+def main():
+  SIZE = 10
+
+  node = Node(0)
+  for i in range(1, SIZE):
+   node = Node(i, node)
+
+  print(node)
+
+
+if __name__ == "__main__":
+  main()
